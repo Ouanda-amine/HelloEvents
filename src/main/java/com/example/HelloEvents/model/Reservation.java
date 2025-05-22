7ba851a727163201a849901f1e4d4f042ff9df6b
amiine <amineouanda@gmail.com>
2025-05-22 11:22:15 +0100

    la partie Reservation

--- src/main/java/com/example/HelloEvents/model/Reservation.java
+++ src/main/java/com/example/HelloEvents/model/Reservation.java
@@ -1,13 +1,17 @@
 package com.example.HelloEvents.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Reservation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
